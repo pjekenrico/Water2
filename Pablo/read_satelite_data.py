413fcb05f820757333b4a2a92d1025016694ba59
Pablo Jeken <pablojeken@hotmail.com>
2020-04-24 14:57:58 +0200

    Adapted visualization. Now callable as an object on any dataset. Write little script to read in satelite data. TODO: patch ill values of the data and reduce its size.

---
+++ Pablo/read_satelite_data.py
@@ -0,0 +1,87 @@
+﻿import netCDF4
+from netCDF4 import Dataset
+import os
+import numpy as np
+import datetime as dt
+import matplotlib
+import matplotlib.pyplot as plt
+from itertools import compress
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
+from cartopy.mpl.gridliner import LONGITUDE_FORMATTER, LATITUDE_FORMATTER
+
+
+def readSatData(path):
+    '''
+        Reads in NetCDF4 files from the given path and returns them as a numpy matrix.
+        Outputs the longitude and latitude matrices.
+    '''
+    dataset = Dataset(path, mode = 'r')
+    key = list(dataset.variables)[0]
+    print("\nReading in file at: " + path + "\nQuantity: " + key)
+
+    time = dataset.variables['time']
+
+    d = [dt.date(dd.year, dd.month, dd.day) for dd in netCDF4.num2date(time[:], time.units)]
+
+    try:
+        lons = dataset.variables['lon'][:]
+        lats = dataset.variables['lat'][:]
+    except:
+        lons = dataset.variables['longitude'][:]
+        lats = dataset.variables['latitude'][:]
+
+    data = np.squeeze(dataset.variables[key][:])
+
+    dataset.close()
+
+    print("Domain coordinates: " + str((np.min(lats), np.max(lats))) +", "+ str((np.min(lons), np.max(lons))))
+    print("Domain dimensions (lat, lon): " + str(lons.shape))
+    print("Time frame: " + str(d[0]) + " - " +str(d[-1]))
+    print("Number of time steps: " + str(len(d)))
+
+    return data, lons, lats, d
+
+
+
+
+chl_path = os.path.abspath('dataset-CHL-satellite-daily.nc')
+spm_path = os.path.abspath('dataset-SPM-satellite-monthly.nc')
+
+[data, lons, lats, times] = readSatData(spm_path)
+
+
+
+
+lons, lats = np.meshgrid(lons, lats)
+timestep = 100
+
+# Plotting the clusters
+matplotlib.rcParams['figure.figsize'] = (10, 10)
+proj = ccrs.Mercator()
+m = plt.axes(projection=proj)
+
+# Put a background image on for nice sea rendering.
+m.stock_img()
+m.coastlines(resolution='10m')
+m.add_feature(cfeature.BORDERS)
+gl = m.gridlines(crs=ccrs.PlateCarree(), draw_labels=True,
+                 linewidth=2, color='gray', alpha=0.5, linestyle='--')
+gl.xformatter = LONGITUDE_FORMATTER
+gl.yformatter = LATITUDE_FORMATTER
+gl.xlabels_top = False
+gl.ylabels_right = False
+
+# Plot data
+plt.contourf(lons, lats, data[timestep,:,:], 50, transform=ccrs.PlateCarree())
+
+# Add Colorbar
+cbar = plt.colorbar()
+
+# Add Title
+plt.title('Time: ' + str(times[timestep]) + ' clusters')
+
+plt.show()
+
+# np.save('sat_model_data.npy', matrix)
+#np.save('lons_lats.npy', lons_lats)
--- Pablo/visualization.py
+++ Pablo/visualization.py
@@ -12,9 +12,10 @@ from cartopy.mpl.gridliner import LONGITUDE_FORMATTER, LATITUDE_FORMATTER
 from matplotlib import cm
 import string
 
+
 def clean_up_artists(axis, artist_list):
     """
-    try to remove the artists stored in the artist list belonging to the 'axis'.
+    Try to remove the artists stored in the artist list belonging to the 'axis'.
     :param axis: clean artists belonging to these axis
     :param artist_list: list of artist to remove
     :return: nothing
@@ -43,27 +44,25 @@ def clean_up_artists(axis, artist_list):
 
 
 def update_plot(frame_index, data_list, lons, lats, fig, axis, n_cols, n_rows,\
-                    number_of_contour_levels, v_min, v_max,changed_artists):
+                    number_of_contour_levels, v_min, v_max,changed_artists, d, keys, units):
     """
     Update the the contour plots of the time step 'frame_index'
 
-    :param frame_index: integer required by animation running from 0 to n_frames -1.
+    :param frame_index: Integer required by animation running from 0 to n_frames -1.
     For initialisation of the plot call 'update_plot' with frame_index = -1
-    :param data_list: list with the 3D data (time x 2D data) per subplot
+    :param data_list: List with the 3D data (time x 2D data) per subplot
     :param lons: Longitude degrees
     :param lats: Latitude degrees
-    :param fig: reference to the figure
-    :param axis: reference to the list of axis with the axes per subplot
-    :param n_cols: number of subplot in horizontal direction
-    :param n_rows: number of subplot in vertical direction
-    :param number_of_contour_levels: number of contour levels
-    :param v_min: minimum global data value. If None take min(data) in the 2d dataset
-    :param v_max: maximum global data value. If None take the largest value in the 2d data set
-    :param changed_artists: list of lists of artists which are updated between the time steps
-    :return: the changed_artists list
+    :param fig: Reference to the figure
+    :param axis: Reference to the list of axis with the axes per subplot
+    :param n_cols: Number of subplot in horizontal direction
+    :param n_rows: Number of subplot in vertical direction
+    :param number_of_contour_levels: Number of contour levels
+    :param v_min: Minimum global data value. If None take min(data) in the 2d dataset
+    :param v_max: Maximum global data value. If None take the largest value in the 2d data set
+    :param changed_artists: List of lists of artists which are updated between the time steps
+    :return: Changed_artists list
     """
-    # Constant data specifications
-    global d, keys, units
 
     # Number of current subplot
     nr_subplot = 0
@@ -140,8 +139,177 @@ def update_plot(frame_index, data_list, lons, lats, fig, axis, n_cols, n_rows,\
 
 
 
+class TimeSeries():
+
+    def __init__(self, data, lons, lats, keys = None, units = None, d = None):
+        '''
+        Load data for animation
+            :param data: List of masked arrays, where each one contains a certain quantity.
+            :param lons: Longitude coordinates as vector or matrix with same sized lats matrix.
+            :param lats: Latitude coordinates as vector or matrix with same sized lons matrix.
+            :param keys: Quantity label (Oxygen, Nitrate, Phosphate, ...).
+            :param units: Unit of the displayed quantity as a list of strings.
+            :param d: List of time date entries. Will be plotted after date on the animation.
+            :return: nothing
+        '''
+        # Load content
+        self.data = data
+        self.keys = keys
+
+        # Check if given lons and lats are 2D or 1D -> then call meshgrid
+        try:
+            lons.shape[1]
+            lats.shape[1]
+            self.lons = lons
+            self.lats = lats
+        except IndexError:
+            [self.lons, self.lats] = np.meshgrid(lons,lats)
+
+
+        # Check if keys are given
+        if keys is None:
+            self.keys = list()
+            for i in range(len(data)):
+                self.keys.append(str(i))
+        else:
+            self.keys = keys
+
+        # Check if units are given
+        if units is None:
+            self.units = list()
+            for i in range(len(data)):
+                self.units.append(" ")
+        else:
+            self.units = units
+
+        # Check if a date is given
+        if d is None:
+            self.d = list()
+            for i in range(len(data[0])):
+                self.d.append(i)
+        else:
+            self.d = d
+
+        print("Domain coordinates: " + str((np.min(self.lats), np.max(self.lats))) +", "+ str((np.min(self.lons), np.max(self.lons))))
+        print("Domain dimensions (lat, lon): " + str(self.lons.shape))
+        print("Time frame: " + str(self.d[0]) + " - " +str(self.d[-1]))
+        print("Number of time steps: " + str(len(self.d)))
+
+
+    def createAnimation(self, number_of_contour_levels = 10, n_rows = 2, n_cols = 2,\
+       max_data_value = None, min_data_value = None, start_frame = None, end_frame = None, skip_frames = None):
+        '''
+        Create animation with the data given at init
+            :param max_data_value: Maximal value of each plot e.g. [21, 380, 290, 18]
+            :param min_data_value: Minimal value of each plot e.g. [0, 180, 0, 0]
+
+            :param number_of_contour_levels: Number of colours/contour levels to be displayed
+            :param n_rows: Amount of subplots in a row
+            :param n_cols: Amount of subplots in a column
+            :return: nothing
+        '''
+
+        # Image sizes
+        n_pixels_x,n_pixels_y = self.lons.shape
+
+
+        # Check on given input
+        if n_rows * n_cols != len(self.data):
+            print("Amount of intended subplots ({}·{}) does not match the amount of data sets given ({})".format(n_rows,n_cols,len(self.data)))
+
+
+        # Set frame control
+        if start_frame is None:
+            start_frame = 0
+
+        if end_frame is None:
+            end_frame = len(self.d)
+
+        if skip_frames is None:
+            skip_frames = 1 # 1 - no skipping
+
+        frames = range(start_frame,end_frame,skip_frames)
+
+
+        # For automatic scaling (Not recommended...)
+        if max_data_value is None:
+            max_data_value = [np.max(dataSet) for dataSet in self.data]
+        if min_data_value is None:
+            min_data_value = [np.max((0,np.min(dataSet))) for dataSet in self.data]
+
+
+        # Figure setup
+        fig, axis = plt.subplots(nrows=n_rows, ncols=n_cols, sharex=True, sharey=True,\
+           figsize=(12,8))
+        #, subplot_kw={'projection': ccrs.Mercator()}
+        ## TODO: No compatibility of the projections (maps, borders) with animation
+    
+        fig.subplots_adjust(wspace=0.05, left=0.08, right=0.98)
+
+        ## TODO: No compatibility of the projections (maps, borders) with animation
+        #axis[0][0].stock_img()
+        #axis[0][0].coastlines(resolution='10m')
+        #axis[0][0].add_feature(cfeature.BORDERS)
+
+        #axis[0][1].stock_img()
+        #axis[0][1].coastlines(resolution='10m')
+        #axis[0][1].add_feature(cfeature.BORDERS)
+
+        #axis[1][0].stock_img()
+        #axis[1][0].coastlines(resolution='10m')
+        #axis[1][0].add_feature(cfeature.BORDERS)
+
+        #axis[1][1].stock_img()
+        #axis[1][1].coastlines(resolution='10m')
+        #axis[1][1].add_feature(cfeature.BORDERS)
+
+        changed_artists = list()
+
+        # create first image by calling update_plot with frame_index = -1
+        changed_artists = update_plot(-1, self.data, self.lons, self.lats, fig, axis, n_cols, n_rows,\
+                number_of_contour_levels, min_data_value, max_data_value, changed_artists, self.d, self.keys, self.units)
+
+        print("\nProcessing animation...")
+
+        # Call the animation function. The fargs argument equals the parameter list of update_plot,
+        # except the 'frame_index' parameter.
+        self.ani = animation.FuncAnimation(fig, update_plot,frames=frames,
+                                           fargs=(self.data, self.lons, self.lats, fig, axis, n_cols, n_rows,\
+                                                number_of_contour_levels, min_data_value,\
+                                                max_data_value, changed_artists, self.d, self.keys, self.units),\
+                                           blit=False, repeat=False)
+
+
+    def saveAnimation(self, fps = 8, name = 'toLazytoName', showAnim = True):
+        '''
+        Save animation after computing it with createAnimation
+            :param fps: Amount of frames displayed each second in the video e.g. 10.
+            :param name: Name of the video. Is stored depending on the call path of the object.
+            :return: nothing
+        '''
+        if not hasattr(self, 'ani'):
+            print("No animation available. Please call createAnimation on the object before saving it.")
+        else:
+            print("Saving animation...")
+            Writer = animation.writers['ffmpeg']
+            writer = Writer(fps=fps, metadata=dict(artist='Me'), bitrate=1800)
+            self.ani.save(name+'.mp4', writer=writer)
+
+            if showAnim:
+                plt.show()
+
+
+
+
+
+# Example on how to use the visualization on raw data
+# When calling the class from a separate file import the class as:
+# 
+# from visualization import TimeSeries
+#
+
 def main():
-    global d, keys, units
+
     datasets = []
 
     # Open the four data sets
@@ -172,7 +340,10 @@ def main():
     # Read position data and transform it to a meshgrid
     lons = datasets[0].variables['longitude'][:]
     lats = datasets[0].variables['latitude'][:]
-    lons, lats = np.meshgrid(lons,lats)
+
+    # It is not necessary to calculate the meshgrid, the class can take both
+    # matrix or vector versions of the latitudes and longitudes... 
+    # lons, lats = np.meshgrid(lons,lats)
 
     # Load data and close document
     data = []
@@ -183,67 +354,27 @@ def main():
         datasets[i].close()
 
 
-    # Define values for animation
-    n_pixels_x,n_pixels_y = lons.shape
-    number_of_contour_levels = 20
-    delay_of_frames = 1 # Additional delay between frames
-    n_rows = 2  # number of subplot rows
-    n_cols = 2  # number of subplot columns
-    start_frame = 0
-    end_frame = len(d)
-    skip_frames = 24 # 1 - no skipping
+    # Read data
+    myAnimation = TimeSeries(data, lons, lats, keys = keys, units = units, d = d)
 
-    # For automatic scaling uncomment this ()
-    #max_data_value = [np.max(dataSet) for dataSet in data]
-    #min_data_value = [np.max((0,np.min(dataSet))) for dataSet in data]
+
+    # Set visualization parameters
 
     # User defined value ranges (colour)
     max_data_value = [21, 380, 290, 18]
     min_data_value = [0, 180, 0, 0]
 
-    # Figure setup
-    fig, axis = plt.subplots(nrows=n_rows, ncols=n_cols, sharex=True, sharey=True,\
-       figsize=(12,8))
-    #, subplot_kw={'projection': ccrs.Mercator()}
-    ## TODO: No compatibility of the projections (maps, borders) with animation
-    
-    fig.subplots_adjust(wspace=0.05, left=0.08, right=0.98)
+    # Create animation
+    myAnimation.createAnimation(number_of_contour_levels = 10, n_rows = 2, n_cols = 2,\
+       max_data_value = max_data_value, min_data_value = min_data_value, start_frame = 1000,\
+      end_frame = 2000, skip_frames = 10)
 
-    ## TODO: No compatibility of the projections (maps, borders) with animation
-    #axis[0][0].stock_img()
-    #axis[0][0].coastlines(resolution='10m')
-    #axis[0][0].add_feature(cfeature.BORDERS)
+    # Save animation and view
+    # Note that the playback speed of the animation shown via Python might
+    # not be the same as the one of the stored video (depends on the GPU)
+    myAnimation.saveAnimation(fps = 8, name = 'toLazytoName', showAnim = True)
 
-    #axis[0][1].stock_img()
-    #axis[0][1].coastlines(resolution='10m')
-    #axis[0][1].add_feature(cfeature.BORDERS)
 
-    #axis[1][0].stock_img()
-    #axis[1][0].coastlines(resolution='10m')
-    #axis[1][0].add_feature(cfeature.BORDERS)
-
-    #axis[1][1].stock_img()
-    #axis[1][1].coastlines(resolution='10m')
-    #axis[1][1].add_feature(cfeature.BORDERS)
-
-    changed_artists = list()
-
-    # create first image by calling update_plot with frame_index = -1
-    changed_artists = update_plot(-1, data, lons, lats, fig, axis, n_cols, n_rows,\
-            number_of_contour_levels, min_data_value, max_data_value, changed_artists)
-
-    # Call the animation function. The fargs argument equals the parameter list of update_plot,
-    # except the 'frame_index' parameter.
-    ani = animation.FuncAnimation(fig, update_plot,frames=range(start_frame,end_frame,skip_frames),
-                                  fargs=(data, lons, lats, fig, axis, n_cols, n_rows,\
-                                     number_of_contour_levels, min_data_value,\
-                                         max_data_value, changed_artists),\
-                                  interval=delay_of_frames, blit=False, repeat=False)
-
-    Writer = animation.writers['ffmpeg']
-    writer = Writer(fps=8, metadata=dict(artist='Me'), bitrate=1800)
-    ani.save('Fulldata10fps.mp4', writer=writer)
-    plt.show()
-
+# Execute main only if the script is run directly
 if __name__ == "__main__":
     main()
